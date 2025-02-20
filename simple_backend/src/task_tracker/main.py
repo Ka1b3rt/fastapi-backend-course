bdd8119be041a30ed66d1210a50fd274d0e4d600
ka1b3rt <katbert13@gmail.com>
2025-02-20 15:49:10 +0500

    update task_tracker join json

--- simple_backend/src/task_tracker/main.py
+++ simple_backend/src/task_tracker/main.py
@@ -1,37 +1,50 @@
 from fastapi import FastAPI # type: ignore
+import json
 
 app = FastAPI()
-tasks = []
-
-
-
 
 @app.get("/tasks")
 def get_tasks():
+    with open('tasks.json', 'r', encoding = 'utf-8') as f:
+            tasks = json.load(f)
     return tasks
 
 @app.post("/tasks")
 def create_task(task_id: int, title: str, status: bool):
+    with open('tasks.json', 'r', encoding='utf-8') as f:
+        tasks = json.load(f)
     task = {
         "task_id": task_id,
         "title": title,
         "status": status
-    }
-    tasks.append(task)
+        }
+    tasks["Tasks"].append(task)
+    with open('tasks.json', 'w', encoding='utf-8') as f:
+        json.dump(tasks, f)
     return task
+
+
 
 @app.put("/tasks/{task_id}")
 def update_task(task_id: int, title: str, status: bool):
-    for task in tasks:
-        if task['task_id'] == task_id:
+    with open('tasks.json', 'r', encoding='utf-8') as f:
+        tasks = json.load(f)    
+    for task in tasks['Tasks']:
+        if task['task_id'] == (task_id):
             task['title'] = title
             task['status'] = status
-            return task
+    with open('tasks.json', 'w', encoding='utf-8') as f:
+        json.dump(tasks, f)
+    return task
     
 
 @app.delete("/tasks/{task_id}")
 def delete_task(task_id: int):
-    for task in tasks:
+    with open('tasks.json', 'r', encoding='utf-8') as f:
+        tasks = json.load(f)    
+    for task in tasks['Tasks']:
         if task['task_id'] == task_id:
-            tasks.remove(task)
-            return task
+            tasks['Tasks'].remove(task)
+    with open('tasks.json', 'w', encoding='utf-8') as f:
+        json.dump(tasks, f)
+    return task
