21532e0fd1a2bd015d42e93cbe969bc61898762a
ka1b3rt <katbert13@gmail.com>
2025-02-20 12:40:01 +0500

    update task_tracker

--- simple_backend/src/task_tracker/main.py
+++ simple_backend/src/task_tracker/main.py
@@ -1,19 +1,37 @@
-from fastapi import FastAPI
+from fastapi import FastAPI # type: ignore
 
 app = FastAPI()
+tasks = []
+
+
+
 
 @app.get("/tasks")
 def get_tasks():
-    pass
+    return tasks
 
 @app.post("/tasks")
-def create_task(task):
-    pass
+def create_task(task_id: int, title: str, status: bool):
+    task = {
+        "task_id": task_id,
+        "title": title,
+        "status": status
+    }
+    tasks.append(task)
+    return task
 
 @app.put("/tasks/{task_id}")
-def update_task(task_id: int):
-    pass
+def update_task(task_id: int, title: str, status: bool):
+    for task in tasks:
+        if task['task_id'] == task_id:
+            task['title'] = title
+            task['status'] = status
+            return task
+    
 
 @app.delete("/tasks/{task_id}")
 def delete_task(task_id: int):
-    pass
+    for task in tasks:
+        if task['task_id'] == task_id:
+            tasks.remove(task)
+            return task
